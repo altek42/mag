60b7d4aba65387e0508171f6c9932a971604c6f6
Przemysław Gawlas <prgawlas@gmail.com>
2021-07-18 17:57:44 +0200

    TEX - test - il compare end; begin test alg1

--- src/test/alg1/program.js
+++ src/test/alg1/program.js
@@ -1,20 +1,19 @@
-function swap(arr, firstIndex, secondIndex) {
+function swap(arr, firstIndex, secondIndex){
   var temp = arr[firstIndex];
   arr[firstIndex] = arr[secondIndex];
   arr[secondIndex] = temp;
 }
-function bubbleSortAlgo(arraaytest) {
+function bubbleSortAlgo(arraaytest){
   var len = arraaytest.length,
-    i, j, stop;
-  for (i = 0; i < len; i++) {
-    for (j = 0, stop = len - i - 1; j < stop; j++) {
-      if (arraaytest[j] > arraaytest[j + 1]) {
-        swap(arraaytest, j, j + 1);
+  i, j, stop;
+  for (i=0; i < len; i++){
+    for (j=0, stop=len - i - 1; j < stop; j++){
+      if (arraaytest[j] > arraaytest[j+1]){
+        swap(arraaytest, j, j+1);
       }
     }
-  } return arraaytest;
+  }
+  return arraaytest;
 }
-
 var l = [3, 6, 2, 5, -75, 4, 1];
-var r = bubbleSortAlgo(l);
-console.log(l);
+console.log(bubbleSortAlgo(l));
